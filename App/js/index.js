e70ed880b6840ba7f7bb2ca866c8071a5c33d009
Chandra <52956305+Chandra-Chaganti@users.noreply.github.com>
2022-06-27 15:59:58 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,25 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setLeft("28.19047619047619em")
+                .setTop("4.571428571428571em")
+                .setWidth("9.82857142857143em")
+                .setCaption("Next")
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label7")
+                .setLeft("3.8095238095238093em")
+                .setTop("8.380952380952381em")
+                .setWidth("9.295238095238096em")
+                .setCaption("Label")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
